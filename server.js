59adf6a0c1b8b944a8df35175cf830722f0b27e6
ReFlecT21 <kumaraguru818@gmail.com>
2023-07-13 17:44:10 +0800

    feat: changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { MongoClient } = require("mongodb");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const app = express();
 app.use(bodyParser.json());
