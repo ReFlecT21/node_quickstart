f5607a9c1df1d6b3d7ab8bdfedd9c61ef3b496de
ReFlecT21 <kumaraguru818@gmail.com>
2023-07-15 21:55:23 +0800

    changes

--- server.js
+++ server.js
@@ -126,6 +126,6 @@ app.get("/getAllLocations", async (req, res) => {
     await client.close();
   }
 });
-app.listen(process.env.PORT || 3000, () => {
+server.listen(process.env.PORT || 3000, () => {
   console.log("Server listening on port 3000");
 });
