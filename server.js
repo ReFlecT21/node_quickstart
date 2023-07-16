b19ff785d7036affe6036d5abb70d2adb44a4a0b
ReFlecT21 <kumaraguru818@gmail.com>
2023-07-16 11:33:08 +0800

    cecec

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ io.on("connection", (socket) => {
       );
       const result = await collection.insertOne(marker);
       socket.emit("log", "Inserted marker into database");
-      socket.emit("newMarker", { ...marker, id: result.insertedId });
+      socket.emit("newMarker", { ...marker });
     } catch (e) {
       console.error(e);
     } finally {
