c7b91f7b4908fb39664e0f7322a1ccf922b5e04c
ReFlecT21 <kumaraguru818@gmail.com>
2023-07-22 18:01:00 +0800

    tyfg

--- server.js
+++ server.js
@@ -54,32 +54,22 @@ async function startServer() {
         console.error(e);
       }
     });
+
+    socket.on("removeMarker", async (markerId) => {
+      try {
+        const result = await collection.deleteOne({
+          _id: new ObjectId(markerId),
+        });
+        io.emit("markerRemoved", markerId);
+      } catch (e) {
+        console.error(e);
+      }
+    });
   });
 }
 
 startServer();
 
-io.on("connection", (socket) => {
-  socket.on("removeMarker", async (markerId) => {
-    try {
-      socket.emit("log", "Connecting to MongoDB database");
-      await client.connect();
-      socket.emit("log", "Connected to MongoDB database");
-      const database = client.db("FOMO");
-      const collection = database.collection("locations");
-      socket.emit("log", `Removing marker with ID ${markerId} from database`);
-      const result = await collection.deleteOne({
-        _id: new ObjectId(markerId),
-      });
-      socket.emit("log", "Removed marker from database");
-      io.emit("markerRemoved", markerId);
-    } catch (e) {
-      console.error(e);
-    } finally {
-      await client.close();
-    }
-  });
-});
 app.get("/checkAuth", (req, res) => {
   if (req.session.userId) {
     // user is authenticated
