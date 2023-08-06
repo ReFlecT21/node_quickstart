88f94bb9e5d0344298c57f05e5315a5c9c8acd6d
ReFlecT21 <kumaraguru818@gmail.com>
2023-08-06 20:20:35 +0800

    xsxsx

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const awsConfig = {
   region: "us-east-1",
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  bucketName: "fomofinal",
+  bucketName: "fomopics",
 };
 
 const storage = multer.memoryStorage();
