1fc12e2173680fae61b5710741dc35cc034f39ac
yixuan0922 <yixuan0922@hotmail.com>
2023-11-15 02:47:33 +0800

    Fix: Uncommented out Run Scrape Code every hour

--- server.js
+++ server.js
@@ -252,20 +252,20 @@ io.on("connection", (socket) => {
 });
 
 
-// // To run scrape.js code every hour
-// scrapePath = ' node ./scrape.js';
-// // hourly
-// cron.schedule('0 * * * *', () => {
-// //  7 days
-// // cron.schedule('0 0 * * 0', () => {
-//   exec(scrapePath, (error, stdout, stderr) => {
-//     if (error) {
-//       console.error(`Error executing script: ${error}`);
-//       return;
-//     }
-//     console.log(`Script output: ${stdout}`);
-//   });
-// });
+// To run scrape.js code every hour
+scrapePath = ' node ./scrape.js';
+// hourly
+cron.schedule('0 * * * *', () => {
+//  7 days
+// cron.schedule('0 0 * * 0', () => {
+  exec(scrapePath, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing script: ${error}`);
+      return;
+    }
+    console.log(`Script output: ${stdout}`);
+  });
+});
 
 
 io.on("connection", (socket) => {
