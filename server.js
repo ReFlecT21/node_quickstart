af6294a51d5a3ba47bc6cb73a6e93924b2aaf48a
yixuan0922 <yixuan0922@hotmail.com>
2023-11-26 05:45:23 +0800

    Fix: Changed the timing again

--- server.js
+++ server.js
@@ -256,7 +256,7 @@ io.on("connection", (socket) => {
 // To run scrape.js code every hour
 scrapePath = ' node ./scrape.js';
 // hourly
-cron.schedule('35 18 * * 6', () => {
+cron.schedule('50 21 * * 6', () => {
 //  7 days
 // cron.schedule('0 0 * * 0', () => {
   exec(scrapePath, (error, stdout, stderr) => {
